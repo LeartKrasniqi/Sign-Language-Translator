73fcaf553de6a56dc358339131f80fd346c41261
kevinlin6543 <kevin.lin6543@gmail.com>
2020-04-09 19:39:30 -0400

    added server code

---
+++ server/app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './videos/'
+ALLOWED_EXTENSIONS = {'mpg', 'mp4'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/coffee3')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+        return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        '''
+
+if __name__ == '__main__':
+    app.run()
